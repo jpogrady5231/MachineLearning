fc42c7a926fae00c139660ffe1fc83449ac511ab
benjamingar <147571710+benjamingar@users.noreply.github.com>
2023-11-15 16:46:24 -0500

    Update ML_project_code.R
    #need to figure out how to fix the color for plot g_1_1 and g_2

--- ML FInal Project/ML_project_code.R
+++ ML FInal Project/ML_project_code.R
@@ -17,18 +17,37 @@ library(plotmo)
 plot_dat <- loan_data 
 plot_dat$loan_status <-  as.factor(loan_data$loan_status)
 
-g_1 <- ggplot(plot_dat, aes(x = income_annum, fill = loan_status)) + 
-  geom_density(alpha = 0.5) + 
-  theme_set(theme_bw(base_size = 22) ) + 
+library(ggplot2) 
+library(reshape) 
+library(glmnet)  
+library(plotmo) 
+
+
+plot_dat <- loan_data 
+plot_dat$loan_status <-  as.factor(loan_data$loan_status)
+```
+
+```{r}
+    g_1 <-ggplot(loan_data, aes(x = income_annum)) + # Set X-axis as insurance charges
+  geom_density(fill = "blue", alpha = 0.5) + # Use geom_density to get density plot
+  theme_bw() + # Set theme for plot
+  theme(panel.grid.major = element_blank(), # Turn of the background grid
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank()) 
+g_1 # Generate plot
+
+
+#need to figure out how to fix the color for plot g_1_1 and g_2
+
+g_1_1 <- ggplot(plot_dat, aes(x = cibil_score, fill = loan_status)) + 
+  geom_density(alpha = 0.5) +
+    theme_set(theme_dark(base_size = 22) ) + 
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(), 
         panel.border = element_blank(), 
-        panel.background = element_blank()) + 
-  labs(x = "Annual Income", title = "Annual Income",
-       fill = "loan_status") + 
-  scale_fill_manual(values = c("1" = "red", "0" = "blue"), 
-                    labels = c("1" = "Approved", "0" = "Rejcted")) 
-g_1 
+        panel.background = element_blank()) + labs(x = "Credit SCore", title = "Credit Score - Status",fill = "loan_status")  #+scale_fill_manual(values = c("Approved" = "red", "Rejected" = "blue"), labels = c("Approved","Rejected")) 
+g_1_1 
 
 
 
